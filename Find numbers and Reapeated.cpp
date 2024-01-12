d9fcb2a66098179b2ab6fb062a581764f7bcdcc4
ramsy <46376329+ramzxy@users.noreply.github.com>
2024-01-12 13:52:16 +0330

    Create Find numbers and Reapeated.cpp

---
+++ Find numbers and Reapeated.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+bool search(int[], int, int);
+int repeater(int[], int, int);
+int main()
+{
+	cout << "enter the 5 numbers you want the seacrh to be done in : ";
+	int A[5];
+	for (int i = 0; i < 5; i++)
+		cin >> A[i];
+	cout << "Now enter the number you want to search: ";
+	int Num;
+	cin >> Num;
+	if (search(A, 5, Num))
+		cout << "Given number was found and reapeated for " << repeater(A, 5, Num) << " times";
+	else
+		cout << "Given number was not found.";
+
+};
+
+bool search(int A[], int n, int Num)
+{
+	int beg = 0, end = n - 1;
+	while (beg <= end)
+	{
+		int mid = (beg + end) / 2;
+		if (A[mid] = Num)
+			return 1;
+		else if (A[mid] <= Num)
+			mid = beg + 1;
+		else if (A[mid] >= Num)
+			mid = end - 1;
+		else return 0;
+	};
+}
+
+int repeater(int A[], int n, int Num)
+{
+	int total = 0;
+	for (int i = 0; i < 6; i++)
+	{
+		if (A[i] == Num)
+			total += 1;
+	};
+
+	return total;
+}
