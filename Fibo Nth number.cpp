d632098c74cb56e2babb9bee064e865b911a5bf7
ramsy <46376329+ramzxy@users.noreply.github.com>
2024-01-12 14:05:27 +0330

    Create Fibo Nth number.cpp

---
+++ Fibo Nth number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int);
+int main()
+{
+	cout << "Please enter a positive integer number : ";
+	int n, small = 1, smaller = 1;
+	cin >> n;
+	cout << n << "th number in the fibo sequence is: " << fibo(n);
+};
+
+int fibo(int n)
+{
+	if (n == 1 || n == 2)
+		return 1;
+	int result;
+	result = fibo(n - 1) + fibo(n - 2);
+}
