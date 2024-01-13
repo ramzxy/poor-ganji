252e6d0021f5d4d17baaf6a60405ded8fd086511
ramsy <46376329+ramzxy@users.noreply.github.com>
2024-01-13 15:33:59 +0330

    Sigma male IT india 23

--- BinarySearch-Sorted.cpp
+++ BinarySearch-Sorted.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 using namespace std;
-
-bool booleanSearch(int A[], int, int);
+void sort(int A[], int );
+bool binarySearch(int A[], int, int);
 int main()
 {
 	int A[5], Num;
 	cout << "Enter 5 integer numbers : ";
 	for (int i = 0; i < 5; i++)
 		cin >> A[i];
+	sort(A, 5);
 	cout << "Now enter the number you would like to search for : ";
 	cin >> Num;
-	if (booleanSearch(A, 5, Num))
+	if (binarySearch(A, 5, Num))
 		cout << "Given number was found.";
 	else
 		cout << "Given number was not found.";
+	return 0;
 }
 
-bool booleanSearch(int A[], int n, int Num)
+bool binarySearch(int A[], int n, int Num)
 {
 	int beg = 0, end = n - 1;
 	while (beg <= end)
@@ -29,5 +31,19 @@ bool booleanSearch(int A[], int n, int Num)
 		else if (Num < A[mid])
 			end = mid - 1;
 	}
-	 return false;
+	return false;
+}
+
+void sort(int A[], int n)
+{
+	for (int i = 1; i < n; i++)
+		for (int j = 0; j < n - i; j++)
+			if (A[j] > A[j + 1])
+			{
+				int temp;
+				temp = A[j];
+				A[j] = A[j + 1];
+				A[j + 1] = temp;
+			}
+
 }
